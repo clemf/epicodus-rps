f15fcc3ece6e8a5cadc83c37f76cf478d7e6d805
Luke Chinworth & Clem Freeman <student@epicodus.com>
2015-01-07 12:12:02 -0800

    Added one player functionality to the app, along with tie logic.

--- app.rb
+++ app.rb
@@ -3,13 +3,34 @@ require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/beats?')
 
+get('/') do
+  erb(:game_selection)
+end
+
+get('/one_player') do
+  erb(:one_player)
+end
+
+
 get('/form') do
   erb(:form)
 end
 
 get('/outcome') do
-  result = params.fetch('your_move').beats?('opponent_move')
-  if result
+  if params.fetch('opponent_move') == "computer"
+    computer = ["rock", "paper", "scissors"].sample()
+    result = params.fetch('your_move').beats?(computer)
+    @o_move = computer
+  else
+    result = params.fetch('your_move').beats?('opponent_move')
+    @o_move = params.fetch('opponent_move')
+  end
+
+  @y_move = params.fetch('your_move')
+
+  if @o_move == @y_move
+    @result = "It's a tie!"
+  elsif result
     @result = "You win!"
   else
     @result = "Opponent wins!"
